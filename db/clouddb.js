6ba3f04700667e02337cd0dc37872c4b163a18e2
fangyu.xu <fangyu.xu@olx.com>
2024-02-14 19:40:25 +0100

    update admin

--- db/clouddb.js
+++ db/clouddb.js
@@ -5,9 +5,9 @@
  // mongodb+srv://admin:<password>@cluster0-46e5h.mongodb.net/test?retryWrites=true&w=majority
 
 const DB_USER = "admin"
-const DB_PASSWORD = "admin123"
+const DB_PASSWORD = "fangyuadmin1993"
 const DB_NAME = "acmetravel"
-const CLUSTER_HOST = "cluster0-46e5h.mongodb.net"
+const CLUSTER_HOST = "cluster0.dleslth.mongodb.net"
 
 // Setup the DB URI
 exports.DB_URI= "mongodb+srv://"+DB_USER+":"+DB_PASSWORD+"@"+CLUSTER_HOST+"/"+DB_NAME+"?retryWrites=true&w=majority"
